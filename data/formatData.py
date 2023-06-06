0890708fdbb1dd3168852a50fedf1e73c3877e4f
umamenon24 <uma.menon24@trinityschoolnyc.org>
2023-06-06 10:26:59 -0400

    ms2

--- data/formatData.py
+++ data/formatData.py
@@ -1,17 +1,40 @@
 import json
 
 
-f1 = open("life_expectancy.csv", "r")
+f1 = open("expenses1.csv", "r")
 lines = f1.readlines()
-
 dictionary ={}
 
-# Create the dictionary here
+for i in range(0, len(lines)):
+    lines[i]=lines[i][0:-2]
+line1=lines[0].split(',')
+lines=lines[1::]    
+for line in lines:
+    y=line.split(',')
+    dictionary[y[0]]={}
+    for i in range(1,len(y)):
+        dictionary[y[0]][line1[i]]=y[i]
 
 f1.close()
 
-#Save the json object to a file
-f2 = open("life_expectancy.json", "w")
-json.dump(dictionary, f2, indent = 4)
+f3=open("minwage.csv","r")
+lines=f3.readlines()
 
+for i in range(0, len(lines)):
+    lines[i]=lines[i][0:-2]
+line1=lines[0].split(',')
+lines=lines[1::] 
+for line in lines:
+    y=line.split(',')
+    dictionary[y[0]][y[1]]=[y[2],y[3]]
+
+f3.close()
+print(dictionary)
+
+
+#Save the json object to a file
+f2 = open("data.json", "w")
+json.dump(dictionary, f2, indent = None)
+
+print(f2)
 f2.close()
--- server.py
+++ server.py
@@ -3,17 +3,28 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
+import sys
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
-@app.route('/')
+states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont', 'Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+
+@app.route("/")
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', states=states)
+
+
+@app.route("/macro")
+def macro():
+    return render_template('macro.html', states=states)
+
+# @app.route("/micro/<state>")
+# def micro(state):
+#     return render_template('micro.html', state=state)
+
+@app.route("/micro/<state>")
+def micro(state):
+    return render_template('micro.html', state=state,states=states)
 
 app.run(debug=True)
-
-#@app.route('/')
-@app.route('/macro')
-def macro():
-    return render_template('macro.html')
